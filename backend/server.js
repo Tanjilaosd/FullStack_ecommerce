86413a219822cc5ad96f70053fa8009d93ed7501
TANJILA AKTER <Tanjilajumur24@gmail.com>
2026-02-06 23:22:23 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ connectCloudinary()
 
 
 app.use(cors({
-  origin: ['http://localhost:5174', 'http://localhost:5173'], // React dev server
+  origin: ['https://admin-panel-vab9.onrender.com/', 'https://fronted-1gmi.onrender.com/'], // React dev server
   methods: ['GET','POST','PUT','DELETE'],
   allowedHeaders: ['Content-Type','Authorization'],
   credentials: true
